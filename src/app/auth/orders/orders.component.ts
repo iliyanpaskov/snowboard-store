0067dbf7264d1e8011a1cfe58b5757ac0eee046e
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-06-26 23:10:30 +0300

    Cancel order handler added

--- src/app/auth/orders/orders.component.ts
+++ src/app/auth/orders/orders.component.ts
@@ -1,35 +1,49 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { OrdersService } from 'src/app/services/orders.service';
 import { OrderItem } from 'src/app/shared/interfaces';
 
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+    selector: 'app-orders',
+    templateUrl: './orders.component.html',
+    styleUrls: ['./orders.component.css']
 })
-export class OrdersComponent implements OnInit,DoCheck{
+export class OrdersComponent implements OnInit, DoCheck {
 
-  currentUserOrders: OrderItem[] = [];
-  token: any = this.auth.user?.sessionToken;
-  isLoading:boolean = true;
-  hasOrders:boolean = false;
+    currentUserOrders: OrderItem[] = [];
+    token: any = this.auth.user?.sessionToken;
+    userId: any = this.auth.user?.objectId;
+    isLoading: boolean = true;
+    hasOrders: boolean = false;
+    restUserOrders: OrderItem[] = [];
 
-  constructor(private auth:AuthService ){}
-  
-  ngDoCheck(): void {
-    this.currentUserOrders
-  }
-  
-  ngOnInit(): void {
-    const userData = async () => {
-      const res = await this.auth.currentUser(this.token);
-      this.currentUserOrders = res?.orders; //?
-      this.isLoading = false;
-      if(this.currentUserOrders.length > 0){
-        this.hasOrders = true
-      }
+    constructor(private auth: AuthService, private orders: OrdersService, private router:Router) { }
+
+    cancelOrderHandler(id: string) {
+        this.restUserOrders = this.currentUserOrders.filter(product => product.objectId !== id);
+        const restOrders = async () => {
+            await this.orders.sendOrder(this.userId, this.token, this.restUserOrders);
+            this.router.navigate(['/']);
+        }
+        restOrders();
+
     }
-    userData();
-  }
+
+    ngDoCheck(): void {
+        this.currentUserOrders
+    }
+
+    ngOnInit(): void {
+        const userData = async () => {
+            const res = await this.auth.currentUser(this.token);
+            this.currentUserOrders = res?.orders; //?
+            this.isLoading = false;
+            if (this.currentUserOrders.length > 0) {
+                this.hasOrders = true;
+            }
+        }
+        userData();
+    }
 
 }
--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -5,70 +5,70 @@ import { environment } from 'src/environments/environment';
 const signupURL = environment.apiSignUpURL;
 
 const headers = {
-  'X-Parse-Application-Id': environment.applicationId,
-  'X-Parse-REST-API-Key': environment.apiKey,
-  'X-Parse-Revocable-Session': environment.session,
-  'Content-Type': environment.contentType
+    'X-Parse-Application-Id': environment.applicationId,
+    'X-Parse-REST-API-Key': environment.apiKey,
+    'X-Parse-Revocable-Session': environment.session,
+    'Content-Type': environment.contentType
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class OrdersService {
 
-  counter: number = 0;
-  ordersList: CartProduct[] = [];
-  totalPrice: number = 0;
+    counter: number = 0;
+    ordersList: CartProduct[] = [];
+    totalPrice: number = 0;
 
-  constructor() { }
+    constructor() { }
 
-  addToCart(product: CartProduct): void {
-    this.ordersList.push(product);
-    this.counter = this.ordersList.length;
-    this.totalPrice = 0;
-    this.ordersList.forEach(product => {
-      this.totalPrice += Number(product.price);
-    })
-  }
-
-  removeFromCart(id: string): void {
-    this.ordersList = this.ordersList.filter(product => product.objectId !== id);
-    this.counter = this.ordersList.length;
-    this.totalPrice = 0;
-    this.ordersList.forEach(product => {
-      this.totalPrice += Number(product.price);
-    })
-  }
-
-  async sendOrder(id: string, token: string, orders: CartProduct[]) {
-    const url = `${signupURL}/${id}`
-    try {
-      const res = await fetch(url, {
-        method: "PUT",
-        headers: {
-          'X-Parse-Application-Id': environment.applicationId,
-          'X-Parse-REST-API-Key': environment.apiKey,
-          'X-Parse-Session-Token': token,
-          'Content-Type': environment.contentType
-        },
-        body: JSON.stringify({
-          orders
+    addToCart(product: CartProduct): void {
+        this.ordersList.push(product);
+        this.counter = this.ordersList.length;
+        this.totalPrice = 0;
+        this.ordersList.forEach(product => {
+            this.totalPrice += Number(product.price);
         })
-      })
-      const data = await res.json();
-      if (!data.error) {
-        return data
-      } else {
-        throw data.error
-      }
-    } catch (error) {
-      console.log(error);
     }
-  }
 
-  async cancelOrder() {
-    
-  }
+    removeFromCart(id: string): void {
+        this.ordersList = this.ordersList.filter(product => product.objectId !== id);
+        this.counter = this.ordersList.length;
+        this.totalPrice = 0;
+        this.ordersList.forEach(product => {
+            this.totalPrice += Number(product.price);
+        })
+    }
+
+    async sendOrder(id: string, token: string, orders: CartProduct[]) {
+        const url = `${signupURL}/${id}`
+        try {
+            const res = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    'X-Parse-Application-Id': environment.applicationId,
+                    'X-Parse-REST-API-Key': environment.apiKey,
+                    'X-Parse-Session-Token': token,
+                    'Content-Type': environment.contentType
+                },
+                body: JSON.stringify({
+                    orders
+                })
+            })
+            const data = await res.json();
+            if (!data.error) {
+                return data
+            } else {
+                throw data.error
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async cancelOrder() {
+
+    }
 
 }
