48bc0a991ffca4b57090314b405ef489c93ee7f0
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-07-17 21:08:22 +0300

    Footer date added

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  $time = interval(1000).pipe(
+    map(()=> new Date() )
+  )
 
 }
